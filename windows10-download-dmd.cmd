1e414d61d4b8ec5fc94d2399ae2a27a80818b2bc
Feldwor <BoQsc@users.noreply.github.com>
2021-11-15 15:07:12 +0200

    Add files via upload

---
+++ windows10-download-dmd.cmd
@@ -0,0 +1,17 @@
+@ECHO OFF
+ECHO _____________Extracting .zip file_____________
+mkdir "./7za/"
+curl "https://www.7-zip.org/a/7za920.zip" -O
+move "7za920.zip" "./7za/"
+tar -xf "./7za/7za920.zip" -C "./7za/"
+
+
+ECHO _____________Downloading D language Compiler_____________
+curl "https://s3.us-west-2.amazonaws.com/downloads.dlang.org/releases/2021/dmd.2.098.0.windows.7z" -O
+
+ECHO Extracting .7z file
+"./7za/7za.exe" "x" "./dmd.2.098.0.windows.7z"
+
+ECHO Testing D compiler
+".\dmd2\windows\bin\dmd.exe" --version
+".\dmd2\windows\bin64\dmd.exe" --version
