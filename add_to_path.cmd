0f9a765fe16f7484910e1564b4945726f26b6ad9
Feldwor <BoQsc@users.noreply.github.com>
2021-11-15 20:03:30 +0200

    Update and rename future_add_to_path.cmd to add_to_path.cmd

---
+++ add_to_path.cmd
@@ -0,0 +1,66 @@
+@ECHO OFF
+SETLOCAL EnableDelayedExpansion
+
+
+
+:: Check if Path already exists.
+REM echo "!path!" | findstr /c:"!CURRENT_DIRECTORY!\" >NUL
+REM IF %ERRORLEVEL% EQU 0 (
+REM     ECHO Path already exist.
+REM ) ELSE (
+REM 	ECHO does not exist
+REM )
+
+
+
+REM Ask for elevation of privilegies
+NET SESSION 1>NUL
+IF %ERRORLEVEL% NEQ 0 GOTO ELEVATE
+
+
+REM The Path added to the Environment Path Variable
+SET "CURRENT_DIRECTORY=%~DP0"
+
+REM The program begins
+ECHO Path is being updated.
+
+REM Get previous path folder and save it into variable.
+IF NOT EXIST "!appdata!/previous-dmd-path-environment-variable.txt" GOTO :add_to_path_variable
+	set /p previous=<"!appdata!/previous-dmd-path-environment-variable.txt"
+
+	REM Remove quotes from the variable
+	set "previous_unquoted_variable=!previous:"=!"
+
+	REM Remove path variable from the previous instance of this script.
+	set "path=asd!path:%previous_unquoted_variable%=!"
+
+	
+
+
+
+:add_to_path_variable
+REM Update the path variable
+set "path=!path!;!CURRENT_DIRECTORY!"
+
+REM Overwrite the Path variable in the Windows Registry
+reg.exe "ADD" "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v "Path" /t REG_EXPAND_SZ /d "!path!\" /f
+
+REM Setting a temporary dummy variable so setx will broadcast WM_SETTINGCHANGE so the PATH change is reflected without needing a restart.
+SETX /m DUMMY ""
+REG DELETE "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /F /V DUMMY
+
+REM Keep track of the previous instance Path variable
+echo "%current_directory%"> "%appdata%/previous-dmd-path-environment-variable.txt"
+    
+)
+PAUSE
+
+
+
+
+
+EXIT /B
+:ELEVATE
+CD /d "%~dp0"
+MSHTA "javascript: new ActiveXObject('shell.application').ShellExecute('%~nx0', '', '', 'runas', 1); close();"
+EXIT /B
