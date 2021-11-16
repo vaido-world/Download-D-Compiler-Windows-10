8df42854408e41273e0707fab1607f61f6b4f0e0
Feldwor <BoQsc@users.noreply.github.com>
2021-11-16 17:29:33 +0200

    Update windows10-download-dmd.cmd

--- windows10-download-dmd.cmd
+++ windows10-download-dmd.cmd
@@ -4,6 +4,7 @@
 if "%0" == "%%0" (
 
  curl --location "https://github.com/vaido-world/Download-D-Compiler-Windows-10/raw/main/windows10-download-dmd.cmd" -O
+ pause
  cls
  call windows10-download-dmd.cmd
 )
